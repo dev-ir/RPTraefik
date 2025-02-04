e058700cf6ed412e0846fc4f5caa2f0e1930d0bf
dev-ir <info@developermen.ir>
2025-02-04 16:14:35 +0330

    Update to version 3.3.3

--- install.py
+++ install.py
@@ -49,7 +49,7 @@ def check_requirements():
         subprocess.run(["which", "traefik"], check=True)
     except subprocess.CalledProcessError:
         print(termcolor.colored("Traefik is not installed. Installing Traefik...", "yellow"))
-        run_command(["curl", "-L", "https://github.com/traefik/traefik/releases/download/v3.1.0/traefik_v3.1.0_linux_amd64.tar.gz", "-o", "traefik.tar.gz"])
+        run_command(["curl", "-L", "https://github.com/traefik/traefik/releases/download/v3.3.3/traefik_v3.3.3_linux_amd64.tar.gz", "-o", "traefik.tar.gz"])
         run_command(["tar", "-xvzf", "traefik.tar.gz"])
         run_command(["sudo", "mv", "traefik", "/usr/local/bin/"])
         os.remove("traefik.tar.gz")
