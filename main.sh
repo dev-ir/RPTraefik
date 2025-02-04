52a0efa5b87699d1f71c263b70733b3e9a98dc18
dev-ir <info@developermen.ir>
2025-02-04 16:19:28 +0330

    Update and Ref

--- main.sh
+++ main.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-#add color for text
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 plain='\033[0m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
+PROJECT_DIR="/opt/DVHOST"
 
-cur_dir=$(pwd)
-# check root
 # [[ $EUID -ne 0 ]] && echo -e "${RED}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
 
 install_jq() {
     if ! command -v jq &> /dev/null; then
-        # Check if the system is using apt package manager
         if command -v apt-get &> /dev/null; then
             echo -e "${RED}jq is not installed. Installing...${NC}"
             sleep 1
@@ -28,86 +25,56 @@ install_jq() {
     fi
 }
 
-require_command(){
-    
-    apt update -y && apt upgrade -y
-    apt install python3-pip -y
+loader(){
     install_jq
-    if ! command -v pv &> /dev/null
-    then
-        echo "pv could not be found, installing it..."
-        sudo apt update
-        sudo apt install -y pv
-    fi
+    SERVER_IP=$(hostname -I | awk '{print $1}')
+    SERVER_COUNTRY=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.country')
+    SERVER_ISP=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.isp')
 }
-
 
 menu(){
-    clear
-    # Get server IP
-    SERVER_IP=$(hostname -I | awk '{print $1}')
-    
-    # Fetch server country using ip-api.com
-    SERVER_COUNTRY=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.country')
-    
-    # Fetch server isp using ip-api.com
-    SERVER_ISP=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.isp')
-    
-    echo "+-------------------------------------------------------------------------------------+"
-    echo "| #####    #####    ######   #####     ####    ######   ######   ######   ##  ##      |"
-    echo "| ##  ##   ##  ##     ##     ##  ##   ##  ##   ##       ##         ##     ## ##       |"
-    echo "| ##  ##   ##  ##     ##     ##  ##   ##  ##   ##       ##         ##     ####        |"
-    echo "| #####    #####      ##     #####    ######   #####    #####      ##     ###         |"
-    echo "| ## ##    ##         ##     ## ##    ##  ##   ##       ##         ##     ####        |"
-    echo "| ##  ##   ##         ##     ##  ##   ##  ##   ##       ##         ##     ## ##       |"
-    echo "| ##  ##   ##         ##     ##  ##   ##  ##   ######   ##       ######   ##  ##      |"
-    echo -e "| ${RED}Fire Terafik is a modern HTTP reverse proxy and load balancer${NC}                       |"
-    echo "+-------------------------------------------------------------------------------------+"
-    echo -e "|  Telegram Channel : ${GREEN}@DVHOST_CLOUD ${NC}     |     YouTube : ${GREEN}youtube.com/@dvhost_cloud${NC}    |"
-    echo "+-------------------------------------------------------------------------------------+"
-    echo -e "|${GREEN}Server Country    |${NC} $SERVER_COUNTRY"
-    echo -e "|${GREEN}Server IP         |${NC} $SERVER_IP"
-    echo -e "|${GREEN}Server ISP        |${NC} $SERVER_ISP"
-    echo "+-------------------------------------------------------------------------------------+"
-    echo -e "|${YELLOW}Please choose an option:${NC}"
-    echo "+-------------------------------------------------------------------------------------+"
-    echo -e $1
-    echo "+-------------------------------------------------------------------------------------+"
-    echo -e "\033[0m"
-}
-
-
-loader(){
     
     clear
+    echo "+--------------------------------------------------------------------------+"
+    echo "|  ██████╗ ██████╗ ████████╗██████╗  █████╗ ███████╗███████╗██╗██╗  ██╗    |"
+    echo "|  ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝██║██║ ██╔╝    |"
+    echo "|  ██████╔╝██████╔╝   ██║   ██████╔╝███████║█████╗  █████╗  ██║█████╔╝     |"
+    echo "|  ██╔══██╗██╔═══╝    ██║   ██╔══██╗██╔══██║██╔══╝  ██╔══╝  ██║██╔═██╗     |"
+    echo "|  ██║  ██║██║        ██║   ██║  ██║██║  ██║███████╗██║     ██║██║  ██╗    |"
+    echo "|  ╚═╝  ╚═╝╚═╝        ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝    |"
+    echo "|                                                               ( V3.3.3 ) |" 
+    echo "+--------------------------------------------------------------------------+"
+    echo -e "| Telegram Channel : ${YELLOW}@DVHOST_CLOUD ${NC} |  YouTube : ${RED}youtube.com/@dvhost_cloud${NC} |"
+    echo "+--------------------------------------------------------------------------+"
+    echo -e "${GREEN}|Server Location:${NC} $SERVER_COUNTRY"
+    echo -e "${GREEN}|Server IP:${NC} $SERVER_IP"
+    echo -e "${GREEN}|Server ISP:${NC} $SERVER_ISP"
+    echo "+--------------------------------------------------------------------------+"
+    echo -e "${YELLOW}|"
+    echo -e "|  1  - Install Core and Setup Tunnel"
+    echo -e "|  2  - Display Tunnel Status"
+    echo -e "|  3  - Unistall RPTraefik"
+    echo -e "|  0  - Exit"
+    echo -e "|${NC}"
+    echo "+--------------------------------------------------------------------------+"
     
-    menu "| 1  - Install Tunnel \n| 2  - Uninstall Tunnel \n| 3  - Display Tunnel Status \n| 0  - Exit"
-    
-    read -p "Enter your choice: " choice
+    read -p "Please choose an option: " choice
     
     case $choice in
-        1)
-            python3 install.py install
-            read -p "Press any key to continue..."
-        ;;
-        2)
-            python3 install.py uninstall
-            read -p "Press any key to continue..."
-        ;;
-        3)
-            python3 install.py status
-            read -p "Press any key to continue..."
-        ;;
+        1)  git clone https://github.com/dev-ir/RPTraefik.git
+            python3 install.py install ;;
+        2)  python3 install.py uninstall ;;
+        3)  python3 install.py status ;;
         0)
             echo -e "${GREEN}Exiting program...${NC}"
             exit 0
-        ;;
+            ;;
         *)
-            echo "Invalid choice. Please try again."
-            read -p "Press any key to continue..."
-        ;;
+                echo "Not valid"
+            ;;
     esac
     
 }
-require_command
+
 loader
+menu
